23f97963cd9f6f2162327da3da170fed6eb5b01d
Robert Stolz <robert.stolz87@gmail.com>
2026-01-25 21:14:21 +0100

    Add RealEstate begin

--- frontend/src/components/AddRealEstateCard.tsx
+++ frontend/src/components/AddRealEstateCard.tsx
@@ -1,5 +1,77 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import type {RealEstateModel} from "./model/RealEstateModel.ts";
+import type {PriceType} from "./model/PriceType.ts";
+import type {RoomModel} from "./model/RoomModel.ts";
 
-export default function AddRealEstateCard() {
+type AddRealEstateCardProps = {
+    user:string;
+    handleNewRealEstateSubmit: (newRealEstate: RealEstateModel) => void;
+    language:string;
+}
+
+export default function AddRealEstateCard(props: Readonly<AddRealEstateCardProps>) {
+
+    const [realEstateTitle, setRealEstateTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [address, setAddress] = useState<string>("");
+    const [price, setPrice] = useState<number>(0);
+    const [priceType, setPriceType] = useState<PriceType | null>(null);
+    const [rooms, setRooms] = useState<RoomModel[]>([]);
+    const [totalFloorArea, setTotalFloorArea] = useState<number>(0);
+    const [totalLivingAreaWoFlV, setTotalLivingAreaWoFlV] = useState<number>(0);
+    const [image, setImage] = useState<File | null>(null);
+    const navigate = useNavigate();
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const realEstateData = {
+            realEstateTitle,
+            description,
+            address,
+            price,
+            priceType,
+            rooms,
+            totalFloorArea,
+            totalLivingAreaWoFlV,
+        };
+
+        const data = new FormData();
+
+        if (image) {
+            data.append("image", image);
+        }
+
+        data.append("realEstateModel", new Blob(
+            [JSON.stringify(realEstateData)],
+            {type: "application/json"}
+        ));
+
+        axios
+            .post("/api/real-estate-hub", data, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            })
+            .then((response) => {
+                props.handleNewRealEstateSubmit(response.data);
+                navigate(`/real-estate/${response.data.id}`);
+            })
+            .catch((error) => {
+                alert("An unexpected error occurred. Please try again.");
+                console.error(error);
+            });
+    }
+
+    function onFileChange(e: React.ChangeEvent<HTMLInputElement>) {
+        if (e.target.files) {
+            const file = e.target.files[0];
+            setImage(file);
+        }
+    }
+
     return (
         <h2>
             Add Real Estate Card Component Placeholder
--- frontend/src/components/Favorites.tsx
+++ frontend/src/components/Favorites.tsx
@@ -1,8 +1,46 @@
+import {useEffect, useState} from "react";
+import type {RealEstateModel} from "./model/RealEstateModel.ts";
+import RealEstateCard from "./RealEstateCard.tsx";
+import axios from "axios";
 
-export default function Favorites(){
+type FavoritesProps = {
+    user: string;
+    favorites: string[];
+    toggleFavorite: (realEstateId: string) => void;
+    language: string;
+}
+
+export default function Favorites(props: Readonly<FavoritesProps>) {
+    const [favoritesRealEstates, setFavoritesRealEstates] = useState<RealEstateModel[]>([]);
+
+    useEffect(() => {
+        axios
+            .get(`/api/users/favorites`)
+            .then((response) => {
+                setFavoritesRealEstates(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [props.user, props.favorites]);
+
     return (
-        <h2>
-            Favorites Component Placeholder
-        </h2>
+        <div className="real-estate-card-container">
+            {favoritesRealEstates.length > 0 ? (
+                favoritesRealEstates.map((r: RealEstateModel) => (
+                    <RealEstateCard
+                        key={r.id}
+                        realEstate={r}
+                        user={props.user}
+                        favorites={props.favorites}
+                        toggleFavorite={props.toggleFavorite}
+                        language={props.language}
+                    />
+                ))
+            ) : (
+                <p>No Real Estates in favorites</p>
+            )}
+        </div>
     );
 }
+
