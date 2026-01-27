1662f916472cb37ba50c8a9ae304e61c8cfe67a4
Robert Stolz <robert.stolz87@gmail.com>
2026-01-27 12:03:00 +0100

    my real estate props and const

--- frontend/src/components/MyRealEstates.tsx
+++ frontend/src/components/MyRealEstates.tsx
@@ -1,8 +1,38 @@
+import type {RealEstateModel} from "./model/RealEstateModel.ts";
+import {useState} from "react";
+import {useLocation} from "react-router-dom";
 
-export default function MyRealEstates(){
+type MyRealEstatesProps = {
+    user: string;
+    favorites: string[];
+    toggleFavorite: (realEstateId: string) => void;
+    isEditing: boolean;
+    setIsEditing: React.Dispatch<React.SetStateAction<boolean>>;
+    handleUpdateRealEstate: (updatedRealEstate: RealEstateModel) => void;
+    handleDeleteRealEstate: (realEstateId: string) => void;
+    language: string;
+}
+
+export default function MyRealEstates(props: Readonly<MyRealEstatesProps>) {
+    const [userRealEstate, setUserRealEstate] = useState<RealEstateModel[]>([]);
+    const [editData, setEditData] = useState<RealEstateModel | null>(null);
+    const [image, setImage] = useState<File | null>(null);
+    const [realEstateToDelete, setRealEstateToDelete] = useState<string | null>(null);
+    const [showPopup, setShowPopup] = useState(false);
+    const [imageChanged, setImageChanged] = useState(false);
+    const [imageDeleted, setImageDeleted] = useState(false);
+
+    const [searchQuery, setSearchQuery] = useState<string>("");
+    const [filteredRealEstate, setFilteredRealEstate] = useState<RealEstateModel[]>([]);
+
+    const location = useLocation();
+
+
+
     return (
         <h2>
             My Real Estates Component Placeholder
         </h2>
     );
 }
+
