8e6151048f5eb74730c5cc5dcafe2f2094c9ae79
Robert Stolz <robert.stolz87@gmail.com>
2026-01-31 11:12:23 +0100

    favorites

--- frontend/src/components/Favorites.tsx
+++ frontend/src/components/Favorites.tsx
@@ -1,33 +1,19 @@
-import {useEffect, useState} from "react";
 import type {RealEstateModel} from "./model/RealEstateModel.ts";
 import RealEstateCard from "./RealEstateCard.tsx";
-import axios from "axios";
 
 type FavoritesProps = {
     user: string;
     favorites: string[];
+    favoritesRealEstates: RealEstateModel[];
     toggleFavorite: (realEstateId: string) => void;
     language: string;
 }
 
 export default function Favorites(props: Readonly<FavoritesProps>) {
-    const [favoritesRealEstates, setFavoritesRealEstates] = useState<RealEstateModel[]>([]);
-
-    useEffect(() => {
-        axios
-            .get(`/api/users/favorites`)
-            .then((response) => {
-                setFavoritesRealEstates(response.data);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }, [props.user, props.favorites]);
-
     return (
         <div className="real-estate-card-container">
-            {favoritesRealEstates.length > 0 ? (
-                favoritesRealEstates.map((r: RealEstateModel) => (
+            {props.favoritesRealEstates.length > 0 ? (
+                props.favoritesRealEstates.map((r: RealEstateModel) => (
                     <RealEstateCard
                         key={r.id}
                         realEstate={r}
